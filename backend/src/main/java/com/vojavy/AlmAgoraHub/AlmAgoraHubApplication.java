a8a5fbcd5eff979f70d74eb4e4bc6a6d8ff5d5f7
streblychenko <andrii.streblychenko@gist.cz>
2025-03-25 10:05:08 +0100

    backend

---
+++ backend/src/main/java/com/vojavy/AlmAgoraHub/AlmAgoraHubApplication.java
@@ -0,0 +1,13 @@
+package com.vojavy.AlmAgoraHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlmAgoraHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlmAgoraHubApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/vojavy/AlmAgoraHub/AlmAgoraHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vojavy.AlmAgoraHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlmAgoraHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
